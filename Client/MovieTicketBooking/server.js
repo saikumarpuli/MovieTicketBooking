cd12ff2752ad7ca5041f3aa9b32ac106505ae242
saikumarpuli <saikumarpuli9@gmail.com>
2019-06-06 16:17:29 +0530

    add theater component

---
+++ Client/MovieTicketBooking/server.js
@@ -0,0 +1,7 @@
+const express =require('express'),
+  path=require('path');
+const  app =express();
+app.use(express.static('./dist/MovieTicketBooking'));
+app.get('/*', (req,res)=>{
+  res.sendFile(path.join(__dirname,'/dist/MovieTicketBooking/index.html'));})
+app.listen(process.env.PORT||8080,()=>{console.log('server started')})
