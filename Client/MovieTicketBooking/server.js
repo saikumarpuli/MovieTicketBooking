f3760674c4205af953fc0a6c137a4b0c86dabccf
saikumarpuli <saikumarpuli9@gmail.com>
2019-06-13 13:18:11 +0530

    add theater api and model

--- Client/MovieTicketBooking/server.js
+++ Client/MovieTicketBooking/server.js
@@ -3,5 +3,5 @@ const express =require('express'),
 const  app =express();
 app.use(express.static('./dist/MovieTicketBooking'));
 app.get('/*', (req,res)=>{
-  res.sendFile(path.join(__dirname,'/dist/MovieTicketBooking/index.html'));})
+  res.sendFile(path.join(__dirname,'/dist/MovieTicketBooking/index.js.html'));})
 app.listen(process.env.PORT||8080,()=>{console.log('server started')})
