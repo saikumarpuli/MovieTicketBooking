6382a6d06a22044f08a55ddc7aefe9dd126cedd9
saikumarpuli <saikumarpuli9@gmail.com>
2019-06-21 16:04:37 +0530

    Displayed user details

--- Client/MovieTicketBooking/src/app/homepage/homepage.component.ts
+++ Client/MovieTicketBooking/src/app/homepage/homepage.component.ts
@@ -21,34 +21,6 @@ ngOnInit(){
      })
   }
 
-
-
-  imageUrls: (string | IImage)[] = [
-    { url: 'assets/118.jpg', caption: '118 movie', href: '#config' },
-    { url: 'assets/images/f2.jpg',caption: 'f2', clickAction: () => alert('custom click function') },
-    { url: 'assets/images/viswasam.jpg', caption: 'viswasam', href: 'https://www.apple.com/' },
-    {url:'assets/images/urihindi.jpg', caption:'URI'},
-    { url: 'assets/images/captain-marvel.jpg', caption:'Captain Marvel'}
-  ];
-  height: string = '400px';
-  minHeight: string;
-  arrowSize: string = '30px';
-  showArrows: boolean = true;
-  disableSwiping: boolean = false;
-  autoPlay: boolean = true;
-  stopAutoPlayOnSlide: boolean = true;
-  debug: boolean = false;
-  backgroundPosition: string = 'center center';
-  backgroundRepeat: string = 'repeat';
-  showDots: boolean = true;
-  dotColor: string = '#FFF';
-  showCaptions: boolean = true;
-  captionColor: string = '#FFF';
-  captionBackground: string = 'rgba(0, 0, 0, .35)';
-  lazyLoad: boolean = false;
-   hideOnNoSlides: boolean = false;
-  fullscreen: boolean = false;
-
   getdetails(event) {
        this.router.navigate(['/moviedetails',event],{skipLocationChange: true});
        console.log(event)
